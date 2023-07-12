c831b1a9972a4319edaa2e0dfcc2291ce4a97f29
gnahcty <gnahcty@gmail.com>
2023-07-12 16:38:48 +0800

    film test

--- src/pages/front/public/AllFilms.vue
+++ src/pages/front/public/AllFilms.vue
@@ -1,3 +1,41 @@
 <template>
-  All films
+  <div class="q-pa-md" style="max-width: 400px">
+
+    <q-form
+      @submit="onSubmit"
+      @reset="onReset"
+    >
+      <q-input flat
+        v-model="name"
+        label="search"
+      />
+      <div>
+        <q-btn label="Submit" type="submit" color="primary"/>
+        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+      </div>
+    </q-form>
+
+  </div>
 </template>
+
+<script setup>
+import { api } from '../../../boot/axios'
+import { ref } from 'vue'
+
+const name = ref(null)
+
+const onSubmit = async () => {
+  try {
+    console.log(name.value)
+    const { data } = await api.get('/films/', { params: { name: name.value } })
+    console.log(data)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+const onReset = () => {
+  name.value = null
+}
+
+</script>
