df1f2d11a87dcc1b5c723d147435abc75b8bbdce
gnahcty <gnahcty@gmail.com>
2023-07-13 16:30:58 +0800

    mdbtest

--- src/pages/front/public/AllFilms.vue
+++ src/pages/front/public/AllFilms.vue
@@ -1,41 +1,48 @@
 <template>
-  <div class="q-pa-md" style="max-width: 400px">
-
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-    >
-      <q-input flat
-        v-model="name"
-        label="search"
-      />
-      <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
-
-  </div>
+    <q-card class="my-card" flat bordered>
+      <q-img :ratio="3/4" src="https://cdn.quasar.dev/img/mountains.jpg">
+        <div class="absolute-top-right" style="background: transparent; padding: 0;">
+          <q-btn flat round color="grey" icon="add" id="add" class="absolute-top-right"/>
+        </div>
+      </q-img>
+      <q-card-actions align="left">
+        <q-btn flat round color="green" icon="visibility"/>
+        <span style="color: green;">00</span>
+        <q-btn flat round color="yellow" icon="segment"/>
+        <span style="color: goldenrod;">00</span>
+        <q-btn flat round color="red" icon="favorite"/>
+        <span style="color: red;">00</span>
+      </q-card-actions>
+    </q-card>
 </template>
 
 <script setup>
-import { api } from '../../../boot/axios'
-import { ref } from 'vue'
+// import { api } from '../../../boot/axios'
+// import { ref } from 'vue'
 
-const name = ref(null)
+// const name = ref(null)
 
-const onSubmit = async () => {
-  try {
-    console.log(name.value)
-    const { data } = await api.get('/films/', { params: { name: name.value } })
-    console.log(data)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
-const onReset = () => {
-  name.value = null
-}
+// const onSubmit = async () => {
+//   try {
+//     const title = name.value
+//     const { data } = await api.get(`/films/search/:${title}`)
+//     console.log(data)
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
 
 </script>
+<style>
+.my-card{
+  width:200px;
+  border: none;
+}
+span{
+  margin-right: 5px;
+  margin-right: 0px;
+}
+#add:hover{
+  color: white;
+}
+</style>
