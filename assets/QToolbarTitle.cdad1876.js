c5630280a05b890040ddbc582f2f2adf52bbb835
gnahcty <120380571+gnahcty@users.noreply.github.com>
2023-08-22 03:32:50 +0000

    Deploying to gh-pages from @ gnahcty/quasar-project@ad119e00331cfad64cbe87b1396844c8b174987d 🚀

---
+++ assets/QToolbarTitle.cdad1876.js
@@ -0,0 +1,1 @@
+import{z as t,c as a,h as l,J as r}from"./index.94f685bf.js";var n=t({name:"QToolbarTitle",props:{shrink:Boolean},setup(o,{slots:e}){const s=a(()=>"q-toolbar__title ellipsis"+(o.shrink===!0?" col-shrink":""));return()=>l("div",{class:s.value},r(e.default))}});export{n as Q};
--- assets/use-quasar.c7a7117e.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aB as r}from"./index.9f63ecff.js";function u(){return a(r)}export{u};
