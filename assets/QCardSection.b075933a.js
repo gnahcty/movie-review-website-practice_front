17b0a786b0c66e6726d005910997a83b108d7b1a
gnahcty <120380571+gnahcty@users.noreply.github.com>
2023-08-24 00:37:18 +0000

    Deploying to gh-pages from @ gnahcty/quasar-project@1cfb650f7256c7fa4db47ba11de29f1fade29053 🚀

--- assets/QCardSection.b075933a.js
+++
@@ -1,1 +0,0 @@
-import{G as e,c as r,h as n,P as s}from"./index.ec6ff545.js";var i=e({name:"QCardSection",props:{tag:{type:String,default:"div"},horizontal:Boolean},setup(t,{slots:a}){const o=r(()=>`q-card__section q-card__section--${t.horizontal===!0?"horiz row no-wrap":"vert"}`);return()=>n(t.tag,{class:o.value},s(a.default))}});export{i as Q};
--- assets/use-quasar.8e7969dd.js
+++
@@ -1,1 +0,0 @@
-import{i as a,aG as r}from"./index.ec6ff545.js";function u(){return a(r)}export{u};
